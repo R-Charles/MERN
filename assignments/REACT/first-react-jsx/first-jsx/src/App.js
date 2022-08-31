69538b83c99b45a5a1e75c99a32fb7cde2340c0a
R-Charles <Rapcharlo@aol.com>
2022-08-31 13:01:37 -0700

    jsx

--- assignments/REACT/first-react-jsx/first-jsx/src/App.js
+++ assignments/REACT/first-react-jsx/first-jsx/src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hello Dojo!
+      </h1>
+      <h2>
+        Things I need to do:
+      </h2>
+      <ul>Learn React</ul>
+      <ul>Climb Mt. Everest</ul>
+      <ul>Run a marathon</ul>
+      <ul>Feed the dogs</ul>
     </div>
   );
 }
