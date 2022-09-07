a86213a715a96a2f738ca50edfe3ca7ffa65c571
R-Charles <Rapcharlo@aol.com>
2022-09-06 17:56:21 -0700

    fetching

---
+++ assignments/REACT/REACT Routing/pokemon-api/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import FetchPokemon from './components/FetchPokemon';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Pokemon API </h1>
+      <FetchPokemon></FetchPokemon>
+    </div>
+  );
+}
+
+export default App;
---
+++ assignments/REACT/REACT Routing/pokemon-api/src/components/FetchPokemon.jsx
@@ -0,0 +1,46 @@
+import React, { useCallback, useState } from "react";
+
+
+const FetchPokemon = () => {
+
+    let [pokemon, setPokemon] = useState([])
+
+    const getData = () => {
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=807&offset=0")
+            .then(response => {
+                return response.json()
+            })
+            .then(response=> {
+                console.log("response from api is ->", response)
+                setPokemon( response)
+
+            })
+            .catch(err=>{
+                console.log("something went wrong fetching", err )
+            })
+
+    }
+
+    
+
+
+    return(
+        <div>
+            <button onClick={getData}>Click me to fetch recent pokemon data</button>
+            {
+                pokemon.map((pokemon, idx)=>{
+                    return(
+                        <div key={idx}>
+                            <h3>{pokemon.name}</h3>
+                        </div>
+                    )
+                })
+            }
+        </div>
+        
+    )
+}
+
+
+
+export default FetchPokemon;
